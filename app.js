da38a4a6e3f03c66143a65970a6f99da7df2b28a
Leonardo Marquezani <leomarquezani@gmail.com>
2020-02-05 17:53:40 -0300

    Added User logic

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
 
 const app = express();
 
@@ -13,18 +14,17 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
- 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  /*   User.findByPk(1)
+  User.findById("5e3afe97e3faac5e92856ae5")
     .then(user => {
       req.user = user;
       next();
     })
-    .catch(err => console.log(err)); */
-    next();
+    .catch(err => console.log(err));
 });
 
 app.use("/admin", adminRoutes);
