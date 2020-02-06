5c62d8d199e6fd28a2aa66ab9d21e62a8a5db844
Leonardo Marquezani <leomarquezani@gmail.com>
2020-02-06 18:54:59 -0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,9 +5,9 @@ const ObjectId = mongodb.ObjectId;
 
 class User {
   constructor(username, email, cart, id) {
-    this.username = username;
+    this.name = username;
     this.email = email;
-    this.cart = cart; // {items: []}
+    this.cart = cart ? cart : (cart = { items: [] });
     this._id = id;
   }
 
@@ -17,28 +17,46 @@ class User {
   }
 
   addToCart(product) {
-    /*     const cartProducts = this.cart.item.findIndex(cp => {
-      return cp._id === product._id;
-    }); */
-    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    console.log("CARTTTT ", this.cart);
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
 
-    const db = getDb();
-    return db.collection("users").updateOne(
-      { _id: new ObjectId(this._id) },
-      { $set: { cart: updatedCart } }
-    );
-  }
-
-  static findById(id) {
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity
+      });
+    }
+    const updatedCart = {
+      items: updatedCartItems
+    };
     const db = getDb();
     return db
       .collection("users")
-      .findOne({ _id: new ObjectId(id) })
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new ObjectId(userId) })
       .then(user => {
         console.log(user);
         return user;
       })
-      .catch(err => console.log(err));
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
 
