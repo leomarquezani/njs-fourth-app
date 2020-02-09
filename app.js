5c4a51d2f03d9221f3746dc7db5fb65252ecab9e
Leonardo Marquezani <leomarquezani@gmail.com>
2020-02-09 15:39:55 -0300

    Finished mogoose

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-//const User = require("./models/user");
+const User = require("./models/user");
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-/* app.use((req, res, next) => {
-  User.findById("5e3afe97e3faac5e92856ae5")
+app.use((req, res, next) => {
+  User.findOne()
     .then(user => {
-      req.user = new User(user.username, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch(err => console.log(err));
-}); */
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -35,6 +35,18 @@ app.use(errorController.get404);
 mongoose
   .connect("mongodb://localhost:27017/shop")
   .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: "Teste",
+          email: "teste@teste.com",
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch(err => console.log(err));
